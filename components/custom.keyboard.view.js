37c035ea01f8308372cca1af41fc154f212dfab6
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2024-07-19 15:24:53 +0530

    add new chat

--- components/custom.keyboard.view.js
+++ components/custom.keyboard.view.js
@@ -1,16 +1,25 @@
 import {KeyboardAvoidingView, Platform, ScrollView} from "react-native";
 
 const android = Platform.OS === 'android';
-export default function CustomKeyboardView({children}) {
+export default function CustomKeyboardView({children, inChat}) {
+
+    let keyConfig = {};
+    let scrollViewConfig = {};
+
+    if (inChat){
+        keyConfig = {keyboardVerticalOffset: 90}
+        scrollViewConfig = {contentContainerStyle: {flex: 1}}
+    }
     return (
         <KeyboardAvoidingView
             behavior={android ? 'height' : 'padding'}
+            keyboardVerticalOffset={keyConfig.keyboardVerticalOffset || 90}
             style={{flex: 1}}>
             <ScrollView
                 style={{flex: 1}}
                 bounces={false}
-                showsVerticalScrollIndicator={false}
-            >
+                contentContainerStyle={scrollViewConfig.contentContainerStyle}
+                showsVerticalScrollIndicator={false}>
                 {
                     children
                 }
--- components/message.list.js
+++ components/message.list.js
@@ -1,9 +1,9 @@
-import {View} from "react-native";
+import {Text, View} from "react-native";
 
 export default function MessageList(){
     return (
         <View>
-
+            <Text>messahge</Text>
         </View>
     )
 }
