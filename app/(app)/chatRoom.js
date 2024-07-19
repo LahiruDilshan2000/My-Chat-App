31fa9f32a034d27aaa5ed3d39961c1fba08589a3
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2024-07-19 14:06:19 +0530

    add chat header

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -3,15 +3,21 @@ import {useLocalSearchParams, useRouter} from "expo-router";
 import {StatusBar} from "expo-status-bar";
 import ChatRoomHeader from "../../components/chat.room.header";
 
-export default function ChatRoom(){
+export default function ChatRoom() {
 
     const item = useLocalSearchParams();
     const router = useRouter();
 
-    return(
+    return (
         <View className={'flex-1 bg-white'}>
             <StatusBar style={'dark'}/>
             <ChatRoomHeader user={item} router={router}/>
+            <View className={'h-3 border-b border-b-neutral-300'}></View>
+            <View className={'flex-1 justify-between bg-neutral-100 overflow-visible'}>
+                <View className={'flex-1'}>
+                    <MessageList/>
+                </View>
+            </View>
         </View>
     )
 }
